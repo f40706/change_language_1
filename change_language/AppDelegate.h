7cbc39965565bd4a705a90fa2e12889b6622ac8b
john <f40706@gmail.com>
2022-03-22 23:09:44 +0800

    Initial Commit

---
+++ change_language/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  change_language
+//
+//  Created by 王志中 on 2022/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ change_language/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  change_language
+//
+//  Created by 王志中 on 2022/3/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
